103a66b0d0d9c881b136ea518034d0f1a65eb183
Sebastian Hietsch <hietschseb@outlook.com>
2018-05-03 17:02:07 +0200

    Start using v-model on transactions

--- html/js/vueTransactionComponents.js
+++ html/js/vueTransactionComponents.js
@@ -1,20 +1,36 @@
 Vue.component("transaction-component",{
-	props:["transactions","text","language","config","transaction","editMode"],
+	props:["transactions","text","language","config","editMode","transaction"],
+	model: {
+		prop: 'transaction',
+ 		event: 'change'
+ 	},
 	template:`
 		<div class="transaction" v-bind:style="[backgroundColor(transaction)]">
 			<div class="transactionTop">
 				<div class="transactionTitle">
 					<span v-if="!editMode">{{transaction["title"]}}</span>
-					<input v-else type="text" v-model="transaction['title']" :placeholder="text.transactionTitle[language]">
+					<span v-else><input type="text" v-model="transaction['title']" :placeholder="text.transactionTitle[language]"></span>
 				</div>
 
 				<div class="transactionAmount">
-					{{transaction["type"]}}
-					{{addComma(transaction["amount"])}}
+					<span v-if="!editMode">
+						{{transaction["type"]}}
+						{{addComma(transaction["amount"])}}</span>
+					<span v-else>
+						<toggle-switch v-model="transaction.type" :valueOne="'+'" :valueTwo="'-'" colorOne="#ccffcc" colorTwo="#ffcccc"></toggle-switch>
+						<input type="number" step="any" min="0" v-model="config.newTransactionAmount" :placeholder="text.transactionAmount[language]" class="amountInput">
+					</span>
 				</div>
 			</div>
 			<div class="transactionBottom">
-				<div class="category" v-for="(category,index) in transaction['categories']" :key="index+1000">{{category}}</div>
+				<div v-if="!editMode" class="category" v-for="(category,index) in transaction['categories']" :key="index+1000">{{category}}</div>
+				<div v-else>
+					<input type="text" list="categories" :placeholder="text.categories[language]">
+					<datalist id="categories">
+						<option v-for="(category,index) in categories" :key="index" :value="category[0]">{{category[1]}}</option>
+					</datalist>
+					<button>{{text.addCategory[language]}}</button>
+				</div>
 			</div>
 		</div>`,
 	methods:{
@@ -24,14 +40,45 @@ Vue.component("transaction-component",{
 		addComma: function(number){
 			return addDecimalSeparators(number);
 		}
+	},
+	computed:{
+		categories:function(){
+			var categoriesObject = {};
+			var prop;
+
+			for (date in this.transactions){
+				for (transaction in this.transactions[date]){
+					for (cat in this.transactions[date][transaction]["categories"]){
+						prop = this.transactions[date][transaction]["categories"][cat];
+
+						if (!categoriesObject.hasOwnProperty(prop)){
+							categoriesObject[prop] = 1;
+						} else{
+							categoriesObject[prop] += 1;
+						}
+					}
+				}
+			}
+
+			var sortedKeys = Object.keys(categoriesObject).sort(function(a,b){
+				return categoriesObject[b]-categoriesObject[a];
+			});
+
+			var categoriesArray = [];
+			for (var i = 0; i < sortedKeys.length; i++){
+				categoriesArray.push([sortedKeys[i],categoriesObject[sortedKeys[i]]]);
+			}
+			return categoriesArray;
+		}
 	}
 });
 
 Vue.component("date-component",{
-	props:["transactions","text","language","config","date"],
+	props:["transactions","text","language","config","date","editMode"],
 	template:`
 		<div class="date">
-			{{date}} ({{weekday(date)}})
+			<span v-if="!editMode">{{date}} ({{weekday(date)}})</span>
+			<span v-else><input type="date"></span>
 		</div>`,
 	methods:{
 		weekday:function(date){
--- html/js/vueTransactionsDisplayComponents.js
+++ html/js/vueTransactionsDisplayComponents.js
@@ -3,10 +3,12 @@ Vue.component("transactions-display",{
 	template:`
 		<div id="transactionsDisplay" class="box">
 			<box-title>{{text.transactions[language]}}</box-title>
-			<add-transaction v-bind="$root.$data"></add-transaction>
 			<div id="transactions">
+
+				<transaction-component v-model="transactions['2018-04-12'][0]" :editMode="true" v-bind="$root.$data"></transaction-component>
+
 				<div v-for="(date,index) in Object.keys(transactions).sort().reverse()" :key="index" class="dateContainer">
-					<date-component :date="date" :text="text" :language="language"></date-component>
+					<date-component :date="date" :text="text" :language="language" :editMode="false"></date-component>
 					<transaction-component v-for="(transaction, index2) in transactions[date]" :key="index2" :transaction="transaction" :editMode="false" v-bind="$root.$data"></transaction-component>
 					<!--Use v-model to bind transaction and boolean for editmode-->
 				</div>
