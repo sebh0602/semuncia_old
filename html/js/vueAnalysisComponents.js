5200007ec8e41f32122f7d070a4c00834bce96af
Sebastian Hietsch <hietschseb@outlook.com>
2018-06-14 00:37:45 +0200

    Add graph display (sort-of-working)

--- html/js/vueAnalysisComponents.js
+++ html/js/vueAnalysisComponents.js
@@ -1,9 +1,9 @@
 Vue.component("analysis-display",{
 	props:["transactions","text","language","config"],
 	template:`
-		<div class="box">
+		<div class="box" id="analysis">
 			<box-title>{{text.analysis[language]}}</box-title>
-
+			<div id="content"></div>
 		</div>`,
 
 });
--- html/js/vueGraphDisplayComponents.js
+++ html/js/vueGraphDisplayComponents.js
@@ -1,7 +1,40 @@
 Vue.component("graph-display",{
-	props:["transactions","text","language"],
+	props:["transactions","text","language","initialAmount"],
 	template:`
 		<div id="graphDisplay" class="box">
 			<box-title>{{text.graph[language]}}</box-title>
+			<canvas id="graphCanvas"></canvas>
+
 		</div>`,
+	watch:{
+		transactions:function(val){
+			var data = [];
+			var labels = [];
+			var x = this.initialAmount;
+			for (d in this.transactions){
+				for (t in this.transactions[d]){
+					t = this.transactions[d][t];
+					if (t.type == "+"){
+						x += t.amount;
+					} else{
+						x -= t.amount;
+					}
+				}
+				labels.push(d);
+				data.push(x/100);
+			}
+			var ctx = document.getElementById("graphCanvas");
+			var lineChart = new Chart(ctx,{
+				type:"line",
+				data:{
+					labels:labels,
+					datasets:[
+						{
+							data:data,
+						}
+					]
+				}
+			});
+		}
+	}
 });
