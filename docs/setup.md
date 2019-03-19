43ac573de46eaa99ea3ab41dd1efe735e92f5578
Sebastian Hietsch <hietschseb@outlook.com>
2019-03-19 22:44:45 +0100

    Add rudimentary websocket communications

---
+++ docs/setup.md
@@ -0,0 +1,17 @@
+# setup
+This is a guide for those, who want to set up semuncia themselves.
+
+## nginx
+WebSockets
+1. Add ` map $http_upgrade $connection_upgrade {default upgrade;
+'' close;}` outside of the `server` block.
+2. Add `  upstream websocket {server localhost:8080;}` outside of the
+`server` block.
+3. Add `location /ws/ {
+	proxy_pass http://websocket;
+	proxy_http_version 1.1;
+	proxy_set_header Upgrade $http_upgrade;
+	proxy_set_header Connection $connection_upgrade;
+}` in the `server`
+block.
+4. Run `sudo nginx -t` to check the syntax in the configuration file.
--- html/js/vueHeaderComponents.js
+++ html/js/vueHeaderComponents.js
@@ -4,11 +4,20 @@ Vue.component("custom-header",{
 		<div id="header">
 			<div @click="showSideNav" id="showSideNavDiv"><menu-icon></menu-icon></div>
 			<div id="title">Semuncia</div>
-			<div>{{text.sync[language]}}</div>
+			<div @click="showSyncMenu">{{text.sync[language]}}</div>
 		</div>`,
 	methods:{
 		showSideNav:function(){
 			app.config.showSideNav = true;
+		},
+		showSyncMenu:function(){
+			//the function of this function will change soon
+			var hostname = "ws://" + window.location.hostname + "/ws/"; //ws/wss?
+			var wSocket = new WebSocket(hostname);
+			wSocket.onopen = function(event){
+				wSocket.send(new Date().toISOString());
+				console.log("sent date")
+			}
 		}
 	}
 });
